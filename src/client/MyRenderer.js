8e514a4c44638dab3600db6bceb45353a41a03a7
Gary Weiss <fourvelocity@gmail.com>
2017-10-19 12:12:33 +0300

    pong in single commit

--- src/client/MyRenderer.js
+++ src/client/MyRenderer.js
@@ -11,6 +11,20 @@ export default class MyRenderer extends Renderer {
 
     draw() {
         super.draw();
+
+        for (let objId of Object.keys(this.sprites)) {
+            if (this.sprites[objId].el) {
+                this.sprites[objId].el.style.top = this.gameEngine.world.objects[objId].position.y + 'px';
+                this.sprites[objId].el.style.left = this.gameEngine.world.objects[objId].position.x + 'px';
+            }
+        }
+    }
+
+    addSprite(obj, objName) {
+        if (objName === 'paddle') objName += obj.playerId;
+        this.sprites[obj.id] = {
+            el: document.querySelector('.' + objName)
+        };
     }
 
 }
